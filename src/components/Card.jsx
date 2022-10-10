d962bc71d06ccb6c9eb1e56ad0c58bc9897ae5c6
Rafael Ramos <ingrnrg@gmail.com>
2022-10-09 20:04:47 -0500

    solved responsive bug

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,18 @@
+import Container from './Container'
+
 const Card = ({ data }) => {
   console.log('test de data en card', data)
   return (
-    <div className='card container mt-2' style={{ width: '40rem' }}>
-      <h5 className='card-title mt-2'>{data.title}</h5>
-      <img src={data.hdurl} className='card-img-top mt-0' alt={data.title} />
-      <div className='card-body'>
-        <p id='explanation' className='card-text'>{data.explanation}</p>
-        <p id='copyright'>{data.copyright} &copy; {data.date}</p>
+    <Container>
+      <div className='card container mt-2' style={{ maxWidth: '40rem' }}>
+        <h5 className='card-title mt-2'>{data.title}</h5>
+        <img src={data.hdurl} className='card-img-top mt-0' alt={data.title} />
+        <div className='card-body'>
+          <p id='explanation' className='card-text'>{data.explanation}</p>
+          <p id='copyright'>{data.copyright} &copy; {data.date}</p>
+        </div>
       </div>
-    </div>
+    </Container>
   )
 }
 
